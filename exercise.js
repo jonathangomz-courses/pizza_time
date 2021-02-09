4d3f3254943632b258c54b6776e777a677dc19be
jonathangomz <jonicgp97@gmail.com>
2021-02-09 16:32:26 -0600

    Add base code

--- exercise.js
+++ exercise.js
@@ -1,11 +1,28 @@
-function <function-name>(<some>, <params>) {
+function numberOfPizzas(persons, slicesPerPizza){
   // Your logic goes here...
-  return <something>
 }
 
 (function () {
-  const res = <function-name>(<some>, <args>);
+  const persons = [
+    {
+      name: 'Jony',
+      slices: 6
+    },
+    {
+      name: 'Adrianita',
+      slices: 4
+    },
+    {
+      name: 'Ulises',
+      slices: 3
+    },
+    {
+      name: 'Andres',
+      slices: 5
+    }
+  ];
+  const res = numberOfPizzas(persons, 8);
   console.log(res);
 })()
 
-module.exports = <function-name>;
+module.exports = numberOfPizzas;
